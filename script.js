ad84cee8f82e1186f93a01692517e7a61736f00c
lucas negro <lucas.negro@ghimprove.com>
2024-12-31 03:08:02 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,453 @@
+/*
+  FILM ARRAY: PHASE 1
+ */
+const phase1Movies = [
+    {
+      title: "The Gray Man",
+      genre: "Action",
+      image: "images/movies/the_gray_man.png",
+      recommended: [
+        {
+          title: "Top Gun",
+          genre: "Action, Adventure",
+          image: "images/movies/top_gun.png",
+          recommended: [
+            {
+              title: "Ingloirious Basterds",
+              genre: "Action, Adventure",
+              image: "images/movies/inglorious_basterds.jpg",
+              link: "https://www.imdb.com/title/tt0361748/?ref_=nv_sr_srsg_4_tt_8_nm_0_in_0_q_inglorio"
+            },
+            {
+              title: "Mission: Impossible",
+              genre: "Action, Adventure",
+              image: "images/movies/mission_impossible.jpg",
+              link: "https://www.imdb.com/title/tt0117060/?ref_=nv_sr_srsg_9_tt_8_nm_0_in_0_q_mision"
+            },
+            {
+              title: "Jack Reacher",
+              genre: "Action, Adventure",
+              image: "images/movies/jack_reacher.jpg",
+              link: "https://www.imdb.com/title/tt0790724/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_jack%2520rea"
+            }
+          ]
+        },
+        {
+          title: "Bullet Train",
+          genre: "Action, Comedy",
+          image: "images/movies/bullet_train.png",
+          recommended: [
+            {
+              title: "The Nice Guys",
+              genre: "Action, Comedy",
+              image: "images/movies/the_nice_guys.jpg",
+              link: "https://www.imdb.com/title/tt3799694/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_nice%2520guys"
+            },
+            {
+              title: "Crank",
+              genre: "Action, Comedy",
+              image: "images/movies/crank.jpg",
+              link: "https://www.imdb.com/title/tt0479884/?ref_=nv_sr_srsg_0_tt_5_nm_3_in_0_q_crank"
+            },
+            {
+              title: "Kingsman",
+              genre: "Action, Comedy",
+              image: "images/movies/kingsman.jpg",
+              link: "https://www.imdb.com/title/tt2802144/?ref_=nv_sr_srsg_0_tt_7_nm_1_in_0_q_kingsman"
+            }
+          ]
+        },
+        {
+          title: "Thor: Love & Thunder",
+          genre: "Action, Heroes",
+          image: "images/movies/thor.png",
+          recommended: [
+            {
+              title: "Batman",
+              genre: "Action, Heroes",
+              image: "images/movies/batman.png",
+              link: "https://www.imdb.com/title/tt1877830/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_batman"
+            },
+            {
+              title: "Spiderman",
+              genre: "Action, Heroes",
+              image: "images/movies/spiderman.png",
+              link: "https://www.imdb.com/title/tt0145487/?ref_=nv_sr_srsg_1_tt_7_nm_1_in_0_q_spiderman"
+            },
+            {
+              title: "Flash",
+              genre: "Action, Heroes",
+              image: "images/movies/flash.png",
+              link: "https://www.imdb.com/title/tt0439572/?ref_=nv_sr_srsg_3_tt_8_nm_0_in_0_q_flash"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Blonde",
+      genre: "Drama",
+      image: "images/movies/blonde.png",
+      recommended: [
+        {
+          title: "Purple Hearts",
+          genre: "Drama, Historical",
+          image: "images/movies/purple_hearts.jpg",
+          recommended: [
+            {
+              title: "Schindler's List",
+              genre: "Drama, Historical",
+              image: "images/movies/schindlers_list.jpg",
+              link: "https://www.imdb.com/title/tt0108052/?ref_=nv_sr_srsg_0_tt_3_nm_5_in_0_q_schindler"
+            },
+            {
+              title: "The King's Speech",
+              genre: "Drama, Historical",
+              image: "images/movies/the_kings_speech.jpg",
+              link: "https://www.imdb.com/title/tt1504320/?ref_=nv_sr_srsg_6_tt_8_nm_0_in_0_q_the%2520king%2520spee"
+            },
+            {
+              title: "Lincoln",
+              genre: "Drama, Historical",
+              image: "images/movies/lincoln.jpg",
+              link: "https://www.imdb.com/title/tt0443272/?ref_=nv_sr_srsg_6_tt_3_nm_5_in_0_q_lincoln"
+            }
+          ]
+        },
+        {
+          title: "Black Panther: Wakanda Forever",
+          genre: "Drama, Romance",
+          image: "images/movies/black_panther_wakanda_forever.jpg",
+          recommended: [
+            {
+              title: "La La Land",
+              genre: "Drama, Romance",
+              image: "images/movies/la_la_land.jpg",
+              link: "https://www.imdb.com/title/tt3783958/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_la%2520la%2520land"
+            },
+            {
+              title: "The Notebook",
+              genre: "Drama, Romance",
+              image: "images/movies/the_notebook.jpg",
+              link: "https://www.imdb.com/title/tt0332280/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_the%2520notebook"
+            },
+            {
+              title: "A Star Is Born",
+              genre: "Drama, Romance",
+              image: "images/movies/a_star_is_born.jpg",
+              link: "https://www.imdb.com/title/tt1517451/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_a%2520star%2520is%2520born"
+            }
+          ]
+        },
+        {
+          title: "The Godfather",
+          genre: "Drama, Crime",
+          image: "images/movies/the_godfather.jpg",
+          recommended: [
+            {
+              title: "Goodfellas",
+              genre: "Drama, Crime",
+              image: "images/movies/goodfellas.jpg",
+              link: "https://www.imdb.com/title/tt0099685/?ref_=nv_sr_srsg_0_tt_7_nm_1_in_0_q_goodfellas"
+            },
+            {
+              title: "Scarface",
+              genre: "Drama, Crime",
+              image: "images/movies/scarface.jpg",
+              link: "https://www.imdb.com/title/tt0086250/?ref_=nv_sr_srsg_0_tt_6_nm_2_in_0_q_scarface"
+            },
+            {
+              title: "The Irishman",
+              genre: "Drama, Crime",
+              image: "images/movies/the_irishman.jpg",
+              link: "https://www.imdb.com/title/tt1302006/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_the%2520irishman"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      title: "Minions",
+      genre: "Kids",
+      image: "images/movies/minions.png",
+      recommended: [
+        {
+          title: "Toy Story",
+          genre: "Kids, Adventure",
+          image: "images/movies/toy_story.jpg",
+          recommended: [
+            {
+              title: "Finding Nemo",
+              genre: "Kids, Adventure",
+              image: "images/movies/finding_nemo.jpg",
+              link: "https://www.imdb.com/title/tt0266543/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_finding%2520nemo"
+            },
+            {
+              title: "The Lion King",
+              genre: "Kids, Adventure",
+              image: "images/movies/the_lion_king.jpg",
+              link: "https://www.imdb.com/title/tt6105098/?ref_=nv_sr_srsg_4_tt_7_nm_0_in_0_q_the%2520lion%2520king"
+            },
+            {
+              title: "Monsters, Inc.",
+              genre: "Kids, Adventure",
+              image: "images/movies/monsters_inc.jpg",
+              link: "https://www.imdb.com/title/tt0198781/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_monsters_inc"
+            }
+          ]
+        },
+        {
+          title: "Frozen",
+          genre: "Kids, Musical",
+          image: "images/movies/frozen.jpg",
+          recommended: [
+            {
+              title: "Moana",
+              genre: "Kids, Musical",
+              image: "images/movies/moana.jpg",
+              link: "https://www.imdb.com/title/tt3521164/?ref_=nv_sr_srsg_5_tt_5_nm_1_in_0_q_moana"
+            },
+            {
+              title: "Encanto",
+              genre: "Kids, Musical",
+              image: "images/movies/encanto.jpg",
+              link: "https://www.imdb.com/title/tt2953050/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_encanto"
+            },
+            {
+              title: "Beauty and the Beast",
+              genre: "Kids, Musical",
+              image: "images/movies/beauty_and_the_beast.jpg",
+              link: "https://www.imdb.com/title/tt0101414/?ref_=nv_sr_srsg_3_tt_8_nm_0_in_0_q_beauty%2520and%2520the%2520beast"
+            }
+          ]
+        },
+        {
+          title: "Madagascar",
+          genre: "Kids, Comedy",
+          image: "images/movies/madagascar.jpg",
+          recommended: [
+            {
+              title: "Kung Fu Panda",
+              genre: "Kids, Comedy",
+              image: "images/movies/kung_fu_panda.jpg",
+              link: "https://www.imdb.com/title/tt0441773/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_kung%2520fu%2520panda"
+            },
+            {
+              title: "Shrek",
+              genre: "Kids, Comedy",
+              image: "images/movies/shrek.jpg",
+              link: "https://www.imdb.com/title/tt0126029/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_shrek"
+            },
+            {
+              title: "The Boss Baby",
+              genre: "Kids, Comedy",
+              image: "images/movies/the_boss_baby.jpg",
+              link: "https://www.imdb.com/title/tt3874544/?ref_=nv_sr_srsg_0_tt_8_nm_0_in_0_q_the%2520boss%2520baby"
+            }
+          ]
+        }
+      ]
+    }
+  ];
+  
+  /* ACCESS TO MAIN NODES IN HTML
+  */
+  const headerContainer = document.getElementById("header-container");
+  const phaseContainer = document.getElementById("phase-container");
+  
+  /*
+   * FUNCTION TO RENDER THE HEADER
+   */
+  function renderHeader(phaseNumber) {
+    // We clean the headerContainer so as not to accumulate
+    headerContainer.innerHTML = "";
+  
+    // 1) Logo
+    const logoImg = document.createElement("img");
+    logoImg.src = "images/toroLogo.png";
+    logoImg.alt = "Company Logo";
+    logoImg.classList.add("company-logo");
+  
+    // I add it to the headerContainer
+    headerContainer.appendChild(logoImg);
+  
+    // 2) Progress image. I could do it directly with checkboxes or selection buttons, 
+    // but in my experience, for small projects with little code, the image is more optimized.
+    if (phaseNumber >= 1 && phaseNumber <= 3) {
+      const progressImg = document.createElement("img");
+      progressImg.classList.add("progress-img");
+      progressImg.alt = "Progress " + phaseNumber;
+  
+      if (phaseNumber === 1) {
+        progressImg.src = "images/others/progress1.png";
+      } else if (phaseNumber === 2) {
+        progressImg.src = "images/others/progress2.png";
+      } else if (phaseNumber === 3) {
+        progressImg.src = "images/others/progress3.png";
+      }
+  
+      // Adding the progress image to the headerContainer
+      headerContainer.appendChild(progressImg);
+    }
+  }
+  
+  /*
+    FUNCTION TO RENDER MOVIES (PHASES 1, 2, 3)
+   */
+  function renderMovies(phaseTitle, moviesArray, onMovieClick) {
+    // We create a div that will contain this phase
+    const phaseDiv = document.createElement("div");
+  
+    // Adding a title
+    const title = document.createElement("h2");
+    title.classList.add("question-title");
+    title.textContent = phaseTitle;
+    phaseDiv.appendChild(title);
+  
+    // Cards container
+    const moviesContainer = document.createElement("div");
+    moviesContainer.classList.add("movies-container");
+  
+    // We go through the films of the array
+    moviesArray.forEach((movie) => {
+      const movieCard = document.createElement("div");
+      movieCard.classList.add("movie-card");
+  
+      const movieImg = document.createElement("img");
+      movieImg.src = movie.image;
+      movieImg.alt = movie.title;
+  
+      const movieTitle = document.createElement("div");
+      movieTitle.classList.add("movie-title");
+      movieTitle.textContent = movie.title;
+  
+      const movieGenre = document.createElement("div");
+      movieGenre.classList.add("movie-genre");
+      movieGenre.textContent = movie.genre;
+  
+      // Click event on the movie card
+      movieCard.addEventListener("click", () => {
+        onMovieClick(movie);
+      });
+  
+      movieCard.appendChild(movieImg);
+      movieCard.appendChild(movieTitle);
+      movieCard.appendChild(movieGenre);
+  
+      moviesContainer.appendChild(movieCard);
+    });
+  
+    phaseDiv.appendChild(moviesContainer);
+  
+    return phaseDiv;
+  }
+  
+  /*
+   FUNCTION TO RENDER FINAL PHASE (PHASE 4)
+   */
+  function renderFinalPhase(movie) {
+    // I create a container for the final phase
+    const finalDiv = document.createElement("div");
+    // Assign it an id to be able to give it specific styles in CSS
+    finalDiv.id = "final-phase";
+  
+    // Main title
+    const finalTitle = document.createElement("h2");
+    finalTitle.textContent = "Watch new movies for free!";
+    finalTitle.classList.add("title-celeste");
+    finalDiv.appendChild(finalTitle);
+  
+    // Text 1
+    const subText1 = document.createElement("p");
+    subText1.textContent = "Watch any movies online for free without ads!";
+    finalDiv.appendChild(subText1);
+  
+    // Text 2
+    const subText2 = document.createElement("p");
+    subText2.textContent = "Have fun watching your favourite movies!";
+    finalDiv.appendChild(subText2);
+  
+    // Button
+    const watchButton = document.createElement("a");
+    watchButton.id = "final-button";
+    watchButton.textContent = "Watch here!";
+    watchButton.href = movie.link;
+    watchButton.target = "_blank";
+    finalDiv.appendChild(watchButton);
+  
+    return finalDiv;
+  }
+  
+  /*
+   PHASES
+   */
+  function initPhase1() {
+    // 1) Render header with progress 1
+    renderHeader(1);
+  
+    // 2) Clean the phase container
+    phaseContainer.innerHTML = "";
+  
+    // 3) Create and render the movie view (Phase 1)
+    const phase1View = renderMovies(
+      "Choose one of the three movies",
+      phase1Movies,
+      (selectedMovie) => {
+        // Go to Phase 2
+        initPhase2(selectedMovie);
+      }
+    );
+  
+    phaseContainer.appendChild(phase1View);
+  }
+  
+  function initPhase2(selectedMovieFromPhase1) {
+    // Header with progress 2
+    renderHeader(2);
+  
+    phaseContainer.innerHTML = "";
+  
+    const phase2View = renderMovies(
+      "Which one do you like the most?",
+      selectedMovieFromPhase1.recommended,
+      (selectedMoviePhase2) => {
+        // Go to Phase 3
+        initPhase3(selectedMoviePhase2);
+      }
+    );
+  
+    phaseContainer.appendChild(phase2View);
+  }
+  
+  function initPhase3(selectedMovieFromPhase2) {
+    // Header with progress 3
+    renderHeader(3);
+  
+    phaseContainer.innerHTML = "";
+  
+    const phase3View = renderMovies(
+      "Which one do you like to watch now?",
+      selectedMovieFromPhase2.recommended,
+      (selectedMoviePhase3) => {
+        // Go to Phase 4
+        initPhase4(selectedMoviePhase3);
+      }
+    );
+  
+    phaseContainer.appendChild(phase3View);
+  }
+  
+  function initPhase4(selectedMovieFromPhase3) {
+    // We just display the logo, so we pass a phase number "0"
+    renderHeader(0);
+  
+    phaseContainer.innerHTML = "";
+  
+    const finalView = renderFinalPhase(selectedMovieFromPhase3);
+    phaseContainer.appendChild(finalView);
+  }
+  
+  /*
+   START THE APP IN PHASE 1
+   */
+  initPhase1();
